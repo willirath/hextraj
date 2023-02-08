f2de51babbc0c9dfe2b7d1cda6bfd91ca8226fd2
Willi Rath <wrath@geomar.de>
2023-02-08 14:43:34 +0100

    Fix test

--- tests/test_hexapi.py
+++ tests/test_hexapi.py
@@ -3,8 +3,10 @@ import pytest
 from hextraj.hexapi import HexGrid
 
 
-@pytest.parametrize("orientation", ["flat", "pointy"])
+@pytest.mark.parametrize("orientation", ["flat", "pointy"])
 def test_origin_projected_to_zero(orientation):
-    hexgrid = HexGrid(lon_origin=0, lat_origin=0, hex_size_meters=100, hex_orientation=orientation)
-    hex = hexgrid.point_to_hex(lon=0.0, lat=0.0)
+    hexgrid = HexGrid(
+        lon_origin=0, lat_origin=0, hex_size_meters=100, hex_orientation=orientation
+    )
+    hex = hexgrid.lon_lat_to_hex(lon=0.0, lat=0.0)
     assert hex == (0, 0, 0)
